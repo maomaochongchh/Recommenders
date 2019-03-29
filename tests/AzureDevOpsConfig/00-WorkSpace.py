ffd3d105e6395f4eef7f7ea7f7579b24194a99d7
bethz <6098674+bethz@users.noreply.github.com>
2019-03-28 22:25:30 -0400

    hardcode ws info

--- tests/AzureDevOpsConfig/00-WorkSpace.py
+++ tests/AzureDevOpsConfig/00-WorkSpace.py
@@ -32,11 +32,16 @@ print("SDK Version:", azureml.core.VERSION)
 # print('current dir is ' +os.curdir)
 with open("tests/AzureDevOpsConfig/config.json") as f:
     config = json.load(f)
-
+'''
 workspace_name = config["workspace_name"]
 resource_group = config["resource_group"]
 subscription_id = config["subscription_id"]
 location = config["location"]
+'''
+
+workspace_name = "RecoWS"
+resource_group = "recommender"
+subscription_id = "15ae9cb6-95c1-483d-a0e3-b1a1a3b06324"
 
 cli_auth = AzureCliAuthentication()
 
@@ -64,3 +69,55 @@ except:
 # print Workspace details
 print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep="\n")
 print(ws.getdetails())
+
+
+from azureml.core import Experiment
+experiment_name = 'train-on-amlcompute'
+experiment = Experiment(workspace = ws, name = 'Reco Experiment')
+
+
+from azureml.core.compute import ComputeTarget, AmlCompute
+
+AmlCompute.supported_vmsizes(workspace = ws)
+#AmlCompute.supported_vmsizes(workspace = ws, location='southcentralus')
+
+from azureml.core.runconfig import RunConfiguration
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.runconfig import DEFAULT_CPU_IMAGE
+
+# create a new runconfig object
+run_config = RunConfiguration()
+
+# signal that you want to use AmlCompute to execute script.
+run_config.target = "amlcompute"
+
+# AmlCompute will be created in the same region as workspace
+# Set vm size for AmlCompute
+run_config.amlcompute.vm_size = 'STANDARD_NC6'
+
+# enable Docker 
+run_config.environment.docker.enabled = True
+
+# set Docker base image to the default CPU-based image
+run_config.environment.docker.base_image = DEFAULT_CPU_IMAGE
+
+# use conda_dependencies.yml to create a conda environment in the Docker image for execution
+run_config.environment.python.user_managed_dependencies = False
+
+# auto-prepare the Docker image when used for execution (if it is not already prepared)
+run_config.auto_prepare_environment = True
+
+# specify CondaDependencies obj
+run_config.environment.python.conda_dependencies = CondaDependencies.create(conda_packages=['scikit-learn'])
+
+# Now submit a run on AmlCompute
+from azureml.core.script_run_config import ScriptRunConfig
+
+script_run_config = ScriptRunConfig(source_directory=project_folder,
+                                    script='train.py',
+                                    run_config=run_config)
+
+run = experiment.submit(script_run_config)
+
+# Show run details
+run
