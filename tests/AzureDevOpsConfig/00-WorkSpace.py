84aa1c16b025703432b1860b9dfd933453485f19
bethz <6098674+bethz@users.noreply.github.com>
2019-03-29 09:50:35 -0400

    update 00-workspace.py

--- tests/AzureDevOpsConfig/00-WorkSpace.py
+++ tests/AzureDevOpsConfig/00-WorkSpace.py
@@ -30,6 +30,11 @@ from azureml.core.authentication import AzureCliAuthentication
 from azureml.core import Workspace
 import azureml.core
 
+# this is from https://aidemos.visualstudio.com/DevOps%20for%20AI%20-%20Demo/_git/DevOps-For-AI-AML-Demo?path=%2Faml_service%2F00-WorkSpace.py&version=GBmaster
+# another good ref is https://github.com/Microsoft/MLAKSDeployAML/blob/master/00_AML_Configuration.ipynb
+#
+# Initialize a Workspace
+#
 print("SDK Version:", azureml.core.VERSION)
 # print('current dir is ' +os.curdir)
 with open("tests/AzureDevOpsConfig/config.json") as f:
@@ -70,18 +75,66 @@ except:
 
 # print Workspace details
 print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep="\n")
+    
+#
+# https://github.com/Azure/MachineLearningNotebooks/blob/master/how-to-use-azureml/training/train-on-amlcompute/train-on-amlcompute.ipynb
+# useful experiment, etc info https://docs.microsoft.com/en-us/python/api/overview/azure/ml/intro?view=azure-ml-py
 
-
+#
+# Create an Experiment
+#
 from azureml.core import Experiment
 experiment_name = 'train-on-amlcompute'
-experiment = Experiment(workspace = ws, name = 'Reco Experiment')
+experiment = Experiment(workspace = ws, name = experiment_name)
 
-
+#
+# Check Available VM families
+#
 from azureml.core.compute import ComputeTarget, AmlCompute
 
 AmlCompute.supported_vmsizes(workspace = ws)
 #AmlCompute.supported_vmsizes(workspace = ws, location='southcentralus')
 
+#
+# Create a compute Resource
+# from here https://docs.microsoft.com/en-us/azure/machine-learning/service/tutorial-train-models-with-aml
+#
+from azureml.core.compute import AmlCompute
+from azureml.core.compute import ComputeTarget
+import os
+
+# choose a name for your cluster
+compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME", "cpucluster")
+compute_min_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MIN_NODES", 0)
+compute_max_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MAX_NODES", 4)
+
+# CPU VM = STANDARD_D2_V2. For using GPU VM, set SKU to STANDARD_NC6 
+vm_size = os.environ.get("AML_COMPUTE_CLUSTER_SKU", "STANDARD_NC6")
+
+
+if compute_name in ws.compute_targets:
+    compute_target = ws.compute_targets[compute_name]
+    if compute_target and type(compute_target) is AmlCompute:
+        print('found compute target. just use it. ' + compute_name)
+else:
+    print('creating a new compute target...')
+    provisioning_config = AmlCompute.provisioning_configuration(vm_size = vm_size,
+                                                                min_nodes = compute_min_nodes, 
+                                                                max_nodes = compute_max_nodes)
+
+    # create the cluster
+    compute_target = ComputeTarget.create(ws, compute_name, provisioning_config)
+    
+    # can poll for a minimum number of nodes and for a specific timeout. 
+    # if no min node count is provided it will use the scale settings for the cluster
+    compute_target.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
+    
+     # For a more detailed view of current AmlCompute status, use get_status()
+    print(compute_target.get_status().serialize())
+'''
+#
+# Provision as a run based compute target
+#
 from azureml.core.runconfig import RunConfiguration
 from azureml.core.conda_dependencies import CondaDependencies
 from azureml.core.runconfig import DEFAULT_CPU_IMAGE
@@ -94,6 +147,8 @@ run_config.target = "amlcompute"
 
 # AmlCompute will be created in the same region as workspace
 # Set vm size for AmlCompute
+# chosen from STANDARD_D2_V2 or STANDARD_NC6
+
 run_config.amlcompute.vm_size = 'STANDARD_NC6'
 
 # enable Docker 
@@ -122,3 +177,4 @@ run = experiment.submit(script_run_config)
 
 # Show run details
 run
+'''
