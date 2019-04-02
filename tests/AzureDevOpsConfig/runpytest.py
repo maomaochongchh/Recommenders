cf666a1988722d451f9891159a2430697baeff5d
bethz <6098674+bethz@users.noreply.github.com>
2019-04-02 19:13:52 -0400

    add test junit

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -1,2 +1,3 @@
 import subprocess
-subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and not gpu"])
+#subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and not gpu"])
+subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
