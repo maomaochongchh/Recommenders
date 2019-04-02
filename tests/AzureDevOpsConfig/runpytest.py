750700f341f440e8539f1df456d7adc7bb2df33b
bethz <6098674+bethz@users.noreply.github.com>
2019-04-02 18:40:45 -0400

    all tests

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and not gpu"])
+subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and not gpu"])
