038381997bdcd0b898aedccb8567a8a2722e15d6
bethz <6098674+bethz@users.noreply.github.com>
2019-04-07 22:03:55 -0400

    remove -v from pytest

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -1,3 +1,3 @@
 import subprocess
 #subprocess.run(["python", "-m", "pytest","tests/unit","-v" "-m","not notebooks and not spark and not gpu"])
-subprocess.run(["pytest","tests/unit","-v" "-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml"])
+subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml"])
