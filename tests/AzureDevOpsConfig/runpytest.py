901d40cf71f52fc97c73914d957ec2ce25006a90
bethz <6098674+bethz@users.noreply.github.com>
2019-04-16 17:07:41 -0400

    use test_dataset.py and not gpu

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -3,7 +3,7 @@ from azureml.core import Run
 run = Run.get_context()
 #subprocess.run(["python", "-m", "pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 #subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml","-s", "|","./logs/pytest.log"])
-subprocess.run(["pytest","tests/unit/test_dataset.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml"])
+subprocess.run(["pytest","tests/unit/test_dataset.py","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 
 
 import os
