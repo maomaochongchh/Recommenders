15a647058b6d2547c2d44aa110d85ca59507235e
bethz <6098674+bethz@users.noreply.github.com>
2019-04-16 12:06:26 -0400

    unpin aml version generate conda file.py

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -2,9 +2,10 @@ import subprocess
 from azureml.core import Run
 run = Run.get_context()
 #subprocess.run(["python", "-m", "pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
-subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml"])
+subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml","-s", "|","./logs/pytest.log"])
 
-
+import os
+print("files", os.listdir("."))
 #r
 name_of_upload = "reports"
 path_on_disk = "reports"
