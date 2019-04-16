9538167c573cba2dd2f8510280243c43f9cbdb82
bethz <6098674+bethz@users.noreply.github.com>
2019-04-16 14:13:22 -0400

    remove streaming from pytest output

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -2,10 +2,12 @@ import subprocess
 from azureml.core import Run
 run = Run.get_context()
 #subprocess.run(["python", "-m", "pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
-subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml","-s", "|","./logs/pytest.log"])
+#subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml","-s", "|","./logs/pytest.log"])
+subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml"])
+
 
 import os
-print("files", os.listdir("."))
+print("os.listdir files", os.listdir("."))
 #r
 name_of_upload = "reports"
 path_on_disk = "reports"
