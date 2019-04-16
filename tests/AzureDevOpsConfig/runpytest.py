47cbf64d6aa23b385719701cd1628c50348e15f5
bethz <6098674+bethz@users.noreply.github.com>
2019-04-16 11:22:06 -0400

    add run.getcontext before pytest

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -1,11 +1,11 @@
 import subprocess
 from azureml.core import Run
-
+run = Run.get_context()
 #subprocess.run(["python", "-m", "pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml"])
 
 
-#run = Run.get_context()
-#name_of_upload = "reports"
-#path_on_disk = "reports"
-#run.upload_folder(name_of_upload, path_on_disk)
+#r
+name_of_upload = "reports"
+path_on_disk = "reports"
+run.upload_folder(name_of_upload, path_on_disk)
