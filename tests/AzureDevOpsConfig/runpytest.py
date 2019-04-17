9d480c3031714c1bea3b5d75d7678e68c5971da2
bethz <6098674+bethz@users.noreply.github.com>
2019-04-17 19:01:55 -0400

    Run all gpu unit tests

--- tests/AzureDevOpsConfig/runpytest.py
+++ tests/AzureDevOpsConfig/runpytest.py
@@ -5,7 +5,8 @@ run = Run.get_context()
 print('before subprocess.run')
 #subprocess.run(["python", "-m", "pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 #subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml","-s", "|","./logs/pytest.log"])
-subprocess.run(["pytest","tests/unit/test_dataset.py","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
+#subprocess.run(["pytest","tests/unit/test_dataset.py","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
+subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 
 
 import os
